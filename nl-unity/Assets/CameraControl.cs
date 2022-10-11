2454aa418d8e75b0e5d8ae148b53a6e1919c81d0
Neuheit <38368299+Neuheit@users.noreply.github.com>
2022-10-11 16:52:41 -0400

    Add basic player controls

---
+++ nl-unity/Assets/CameraControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    float XMouse = 0;
+    float YMouse = 0;
+
+    public Transform Player;
+
+    float Sensitivity = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        XMouse += Input.GetAxis("Mouse X") * Sensitivity;
+        YMouse -= Input.GetAxis("Mouse Y") * Sensitivity;
+
+        Debug.Log(XMouse);
+
+        YMouse = Mathf.Clamp(YMouse, -90f, 90f);
+
+        transform.localEulerAngles = new Vector3(YMouse, 0, 0);
+        Player.localEulerAngles = new Vector3(0, XMouse, 0);
+    }
+}
