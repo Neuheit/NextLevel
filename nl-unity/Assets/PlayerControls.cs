c594d3824a85ba0f299e099c1e56c1fdc6c4b59e
SamiAljailani <samialjailani1@gmail.com>
2022-10-13 00:47:30 -0400

    added platform logic

--- nl-unity/Assets/PlayerControls.cs
+++ nl-unity/Assets/PlayerControls.cs
@@ -2,17 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControls : MonoBehaviour
+class HealthSystem
 {
-    // Start is called before the first frame update
-    void Start()
+    private const int initialHealth = 1000;
+    public int CurrentHealth;
+    public int Stamina;
+    HealthSystem()
     {
-        
+        CurrentHealth = initialHealth;
+        Stamina = 100;
+    }
+    int HealthPercentage()
+    {
+        return CurrentHealth / initialHealth;
     }
 
-    // Update is called once per frame
+
+
+
+}
+public class PlayerControls : MonoBehaviour
+{
+    public Collider platformPrefab;
+    void Start()
+    {
+        for(int i = 0; i < 4; ++i)
+        {
+            Collider newPlatform = Instantiate(platformPrefab, transform.position + new Vector3(0, 10.0f*(i+1), 0.0f * i), transform.rotation) as Collider;
+        }
+    }
+
     void Update()
     {
-        
     }
 }
