74943dcedd8213bedd72d09a53a7236a20bbb380
Ben Goodhart <bengoodhart16@gmail.com>
2022-10-29 16:55:09 -0400

    Created scoring system.
    Score is incremented when the player jumps through/on a platform.

---
+++ nl-unity/Assets/PlayerScore.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScore : MonoBehaviour
+{
+    private int score = 0;
+    public Text playerScore;
+
+    void Start()
+    {
+        playerScore.text = "Score: " + score;
+    }
+
+   void Update()
+    {
+        playerScore.text = "Score: " + score;
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "nextLevel")
+        {
+            score += 1;
+            collision.gameObject.tag = "Untagged";
+
+        }
+    }
+}
