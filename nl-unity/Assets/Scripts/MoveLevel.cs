3b644c1a7693217a8ba1a6dfee1e263018df4666
Brokenz-A <79488408+Brokenz-A@users.noreply.github.com>
2022-12-08 15:44:39 -0500

    check Healthbuff

---
+++ nl-unity/Assets/Scripts/MoveLevel.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLevel : MonoBehaviour
+{
+    private Vector3 maxOffset;
+    private Vector3 minOffset;
+    private Vector3 minTarget;
+    private Vector3 maxTarget;
+    private Vector3 currTarget;
+    private Transform player;
+    private bool movePlayer = false;
+    private float   speed;
+    public float    minSpeed = 5;
+    public float    maxSpeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var generator  = GameObject.FindWithTag("LevelGenerator");
+        maxOffset      = generator.GetComponent<LevelGenerator>().maxOffset;
+        minOffset      = generator.GetComponent<LevelGenerator>().minOffset;
+        speed          = Random.Range(minSpeed, maxSpeed);
+        player         = GameObject.FindWithTag("Player").transform;
+        SetTargets();
+    }   
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    void Move(){
+        var newPos = Vector3.MoveTowards(transform.position, currTarget, speed * Time.deltaTime);
+        Vector3 increment = newPos - transform.position;
+
+        MovePlayer(increment);
+        transform.position = newPos;
+
+        // Check if the position of the level and the target is approximately equal
+        if (Vector3.Distance(transform.position, currTarget) < 0.01f)
+        {
+            //toggling between min and maxTarget
+            currTarget = (currTarget == maxTarget) ? minTarget: maxTarget;
+        }
+    }
+    void MovePlayer(Vector3 increment){
+        if(movePlayer){
+            player.position += increment;
+            movePlayer = false;
+        }
+    }
+    void OnCollisionStay(Collision other){
+        if(other.gameObject.CompareTag("Player")){
+            //make sure the player if fully above the level.
+                movePlayer = true;
+        }
+    }
+    void SetTargets(){
+        //randomly choose a axis to move along in x, y, z, or a combinaiton xy, zx, zy
+        minTarget = maxTarget = transform.position;
+        switch (Random.Range(0, 5)){
+            case 0://x
+                minTarget.x += minOffset.x;
+                maxTarget.x += maxOffset.x;
+                break;
+            case 1://y
+                minTarget.y -= minOffset.y;
+                maxTarget.y += maxOffset.y;
+                break;
+            case 2://z
+                minTarget.z += minOffset.z;
+                maxTarget.z += maxOffset.z;
+                break;
+            case 3://zx
+                minTarget.x += minOffset.x; minTarget.z += minOffset.z;
+                maxTarget.x += maxOffset.x; maxTarget.z += maxTarget.z;
+                break;
+            case 4://yz
+                minTarget.y -= minOffset.y; minTarget.z += minOffset.z;
+                maxTarget.y += maxOffset.y; maxTarget.z += maxTarget.z;
+                break;
+            case 5://yx
+                minTarget.y -= minOffset.y; minTarget.x += minOffset.x;
+                maxTarget.y += maxOffset.y; maxTarget.x += maxTarget.x;
+                break;
+        }
+        currTarget = minTarget;
+    }
+
+
+}
---
+++ nl-unity/Assets/Scripts/Player/MovePlayer.cs
@@ -0,0 +1,130 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public AudioSource jumpingAudio;
+    public AudioSource walkingAudio;
+
+    Vector3 moveDir;
+    float speed = 15;
+    float jumpSpeed = 6000f;
+    float gravity = 200f;
+
+    float height;
+
+    Rigidbody rb;
+
+    bool isGrounded;
+    bool isWalking;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    void OnCollisionStay(Collision other) 
+    {
+        //isGrounded = true;
+    }
+
+    // Update is called once per frame
+    void Move()
+    {
+        /*
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out var hit, 1f) && !isGrounded)
+        {
+            height = hit.distance;
+            isGrounded = true;
+        }
+
+        if(isGrounded)
+            transform.position = new Vector3(transform.position.x, height, transform.position.z);
+        */
+
+        var grounded = Physics.Raycast(transform.position, Vector3.down, 3);
+
+        Vector3 dir = new Vector3(0, 0, 0);
+        dir.x = Input.GetAxis("Horizontal");
+        dir.z = Input.GetAxis("Vertical");
+
+        //Debug.Log(dir);
+
+        if(dir != Vector3.zero)
+        {
+            if(!isWalking && grounded)
+            {
+                isWalking = true;
+                walkingAudio.Play();
+            }
+
+            if(!grounded)
+            {
+                walkingAudio.Stop();
+                isWalking = false;
+            }
+
+        }
+        else
+        {
+            if(isWalking)
+            {
+                isWalking = false;
+                walkingAudio.Stop();
+            }
+                
+        }
+
+        transform.Translate(dir * speed * Time.deltaTime);
+        
+        if(Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            rb.AddForce(Vector3.up * speed * 1.5f, ForceMode.Impulse);
+            jumpingAudio.Play();
+        }
+        /*
+        var movX = Input.GetAxis("Horizontal");
+        var movZ = Input.GetAxis("Vertical");
+
+        moveDir = new Vector3(movX, 0, movZ);
+        moveDir = transform.TransformDirection(moveDir);
+
+        moveDir *= speed;
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            moveDir.y = jumpSpeed;
+        }
+
+        moveDir.y -= gravity * speed * Time.deltaTime;
+
+        charCon.Move(moveDir * Time.deltaTime);
+        */
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("BuffBox"))//High Jump Buff
+        {
+            rb.AddForce(new Vector3(0, 45f, 0), ForceMode.VelocityChange);
+            jumpingAudio.Play();
+        }
+        else if(other.gameObject.CompareTag("BuffBox+"))//Super High Jump Buff
+        {
+            rb.AddForce(new Vector3(0, 60f, 0), ForceMode.VelocityChange);
+            jumpingAudio.Play();
+        }
+        else if(other.gameObject.CompareTag("DeBuff"))//Displacement Debuff
+        {
+            rb.transform.position += new Vector3(20f,15f,0);
+            Destroy(other.gameObject);
+        }
+    }
+}
